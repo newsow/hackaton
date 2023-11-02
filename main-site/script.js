ea0bcfd2cb90db826815fa28600fc186523c101a
Deterrent1010 <121808708+Deterrent1010@users.noreply.github.com>
2023-11-02 16:57:27 +0300

    Add files via upload

---
+++ main-site/script.js
@@ -0,0 +1,18 @@
+
+window.onload = function() {
+    var ava = "url('https://static.cdprojektred.com/cms.cdprojektred.com/16x9_big/f4ce7b1be9bc4b0ee2df8f9a311a850a594d3d1a-1920x1080.jpg')";// картинку фигачишь вместо ссылки(в виде переменной, если нужно, всёравно сожрёт)
+    var element = document.getElementById('avatar');
+    element.style.backgroundImage = ava;
+};
+    function FuncImages(number) {   
+        document.getElementById("photoload").style.display = "block";
+        document.getElementById("close-set-photo").style.display = "block";
+        var ava = "url('https://static.cdprojektred.com/cms.cdprojektred.com/16x9_big/f4ce7b1be9bc4b0ee2df8f9a311a850a594d3d1a-1920x1080.jpg')";
+        if(number==0) document.getElementById('image').style.backgroundImage = ava;
+        if(number==1) document.getElementById('image').style.backgroundImage = "url('')";
+        if(number==2) document.getElementById('image').style.backgroundImage = "url('')";
+      }
+      function Funcdialog() {
+        document.getElementById("photoload").style.display = "none";    
+        document.getElementById("close-set-photo").style.display = "none";
+      }
