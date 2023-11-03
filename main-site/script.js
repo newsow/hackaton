6f744a7b2346db61730a21a7d0d9bb54cd853143
Deterrent1010 <121808708+Deterrent1010@users.noreply.github.com>
2023-11-03 18:39:27 +0300

    Update script.js

--- main-site/script.js
+++ main-site/script.js
@@ -7,7 +7,7 @@ window.onload = function() {
     function FuncImages(number) {   
         document.getElementById("photoload").style.display = "block";
         document.getElementById("close-set-photo").style.display = "block";
-        var ava = "url('https://static.cdprojektred.com/cms.cdprojektred.com/16x9_big/f4ce7b1be9bc4b0ee2df8f9a311a850a594d3d1a-1920x1080.jpg')";
+        var ava = "url('')";
         if(number==0) document.getElementById('image').style.backgroundImage = ava;
         if(number==1) document.getElementById('image').style.backgroundImage = "url('')";
         if(number==2) document.getElementById('image').style.backgroundImage = "url('')";
